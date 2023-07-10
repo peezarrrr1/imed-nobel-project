23deb66622ebe3241f68da42d062fbd8a93207e6
Peeraphat <peeraphat48@gmail.com>
2023-07-10 15:45:46 +0700

    Create simple details

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Component/Header'
 import Filters from './Component/Filters'
+import Details from './Component/Details'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-   
     <Header />
-    <div>
+    <div className='flex flex-row' >
     <Filters />
+    <Details />
+    
     </div>
     </div>
   )
