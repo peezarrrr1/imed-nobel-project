295fa9ea691a864752e96ae3b78dda88f78c0ea7
Peeraphat <peeraphat48@gmail.com>
2023-07-10 14:41:31 +0700

    Create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Header from './Component/Header'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <div>
+   
+    <Header />
+    </div>
   )
 }
 
---
+++ src/Component/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+    <div className='bg-yellow-400 h-40 flex justify-center items-center text-black text-3xl'>
+        Nobel Prize
+    </div>
+    
+
+    </div>
+  )
+}
+
+export default Header
