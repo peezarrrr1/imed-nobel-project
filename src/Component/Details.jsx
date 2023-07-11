458e3e5ceed7103c47a2908b89630917e75bd50c
Peeraphat <peeraphat48@gmail.com>
2023-07-11 21:33:18 +0700

    Sent data from component

--- src/Component/Details.jsx
+++ src/Component/Details.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
 
 
-function Details() {
-    const [data, setApi] = useState([])
+function Details(props) {
+    console.log(props.nobelPrizeDetailList)
    
   return (
     <div className='p-5'>
         <div className='bg-gray-300 w-screen h-screen text-3xl'>
             Details
         </div>
-        
+        {props.nobelPrizeDetailList.map((nobelPrizeDetail) => {
+          
+        })}
     </div>
   )
 }
