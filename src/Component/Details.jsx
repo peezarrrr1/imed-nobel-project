cca9d1069045196e6010c5b3af4b62038b700f23
Peeraphat <peeraphat48@gmail.com>
2023-07-12 18:55:28 +0700

    reduce prize Amount

--- src/Component/Details.jsx
+++ src/Component/Details.jsx
@@ -4,38 +4,68 @@ import { Space, Table, Tag } from "antd";
 function Details(props) {
   console.log(props.nobelPrizeDetailList);
 
+  const columns = [
+    {
+      title: "ชื่อรางวัล",
+      dataIndex: "awardName",
+      key: "awardName",
+    },
+    {
+      title: "ปีที่ได้รับรางวัล",
+      dataIndex: "awardYear",
+      key: "awardYear",
+    },
+    {
+      title: "ผู้ที่ได้รับรางวัล",
+      dataIndex: "laureates",
+      key: "laureates",
+    },
+    {
+      title: "แรงบันดาลใจ",
+      dataIndex: "motivation",
+      key: "motivation",
+    },
+  ];
+  let data = [];
+ 
+
+//   data = [
+//     props.nobelPrizeDetailList.map((nobelPrizeDetail) => {
+//       return(
+//         {
+//         awardName: nobelPrizeDetail.category.en,
+//         awardYear: nobelPrizeDetail.awardYear,
+//         laureates: nobelPrizeDetail.awardYear,
+//         motivation: nobelPrizeDetail.category.en,
+//         }
+//       )
+//     })
+//  ,]
+
+  props.nobelPrizeDetailList.map((nobelPrizeDetail, index) => {
+     return (data = [
+      {
+        key:index,
+        awardName: nobelPrizeDetail.category.en,
+        awardYear: nobelPrizeDetail.awardYear,
+        laureates: nobelPrizeDetail.awardYear,
+        motivation: nobelPrizeDetail.category.en,
+      },
+   
+   
+     
+     ]
+    
+    )
+  })
+
+  console.log(data);
+
   return (
     <div className="p-5">
       <div className="bg-gray-300 w-screen h-screen text-3xl">
         Details
-        
-        {props.nobelPrizeDetailList.map((nobelPrizeDetail, index) => {
-          return (
-            <div key={index}>
-              {/* {nobelPrizeDetail.awardYear}////{nobelPrizeDetail.category.en} */}
-              <table>
-                <thead>
-                  <tr>
-                    <th>Month</th>
-                    <th>Savings</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>B</td>
-                    <td>{nobelPrizeDetail.awardYear}</td>
-                  </tr>
-                  <tr>
-                    <td>A</td>
-                    <td>{nobelPrizeDetail.category.en}</td>
-                  </tr>
-                </tbody>
-                
-                </table>
-            </div>
-          );
-        })}
-        
+        <Table columns={columns} dataSource={data} />
       </div>
     </div>
   );
--- src/Component/Filters.jsx
+++ src/Component/Filters.jsx
@@ -1,54 +1,54 @@
-import React, { useRef, useState } from 'react'
-import { Select } from 'antd';
-import { Button} from 'antd';
-import axios from 'axios'
-
+import React, { useRef, useState } from "react";
+import { Select } from "antd";
+import { Button } from "antd";
+import axios from "axios";
 
 function Filters(props) {
-
+  const [sumAmount, setsum] = useState(0);
   const [currentyear, setCurrentYear] = useState(new Date().getFullYear());
   let years = [];
-    for(let i = new Date().getFullYear();i >= 1901;i--){
-      years.push(i);
-      
-    }
-    
-  let awardYearSorted = years.map((result,index)=>{
-    return { value: result , label : result}
-  })
- 
-  // const [yearSelect, take] = useState([]);
-  const getData = async (currentyear) =>{
-    const response = await axios.get(`https://api.nobelprize.org/2.1/nobelPrizes?nobelPrizeYear=${currentyear}`)
-    
-   props.setDetailList(response.data.nobelPrizes);
-   
-   props.setYear(currentyear);
+  for (let i = new Date().getFullYear(); i >= 1901; i--) {
+    years.push(i);
   }
-  
 
-  
-  
- 
+  let awardYearSorted = years.map((result, index) => {
+    return { value: result, label: result };
+  });
+
+  // const [yearSelect, take] = useState([]);
+  const getData = async (currentyear) => {
+    const response = await axios.get(
+      `https://api.nobelprize.org/2.1/nobelPrizes?nobelPrizeYear=${currentyear}`
+    );
+
+    props.setDetailList(response.data.nobelPrizes);
+    const prizeCollect = response.data.nobelPrizes;
+    let sum = prizeCollect.reduce((prev, curr) => prev + curr.prizeAmount, 0);
+
+    props.setYear(currentyear);
+    setsum(sum);
+  };
+
+  // let sum = prizeCollect.reduce((prev, curr) => prev + curr, 0);
+
   return (
-    <div className='p-5'>
-        <div className='bg-blue-400 w-96 h-screen flex justify-center items-center'>
-       
-    <Select
-      defaultValue={currentyear}
-      style={{
-        width: 120,
-      }}
-      onChange={(result) => setCurrentYear(result)}
-      options={
-       awardYearSorted
-      }
-    />
-    <Button className='bg-green-500' onClick={() => getData(currentyear)}>Apply</Button>
-        </div>
-       
-        </div>
-  )
+    <div className="p-5">
+      <div className="bg-blue-400 w-96 h-screen flex justify-center items-center">
+        <Select
+          defaultValue={currentyear}
+          style={{
+            width: 120,
+          }}
+          onChange={(result) => setCurrentYear(result)}
+          options={awardYearSorted}
+        />
+        <Button className="bg-green-500" onClick={() => getData(currentyear)}>
+          Apply
+        </Button>
+        <div>Prize Amount {sumAmount}</div>
+      </div>
+    </div>
+  );
 }
 
-export default Filters
+export default Filters;
